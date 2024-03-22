323d9a225d5e1e892c5e0e0abdd45493cc493760
Patrick Dubroy <pdubroy@gmail.com>
2024-03-22 11:48:50 +0100

    Rework for March release.

--- book.js
+++
@@ -1,9 +0,0 @@
-import * as ohm from 'ohm-js';
-import {extractExamples} from 'ohm-js/extras';
-import {suite} from 'uvu';
-import * as assert from 'uvu/assert';
-
-export function setup(chapterName) {
-  const test = suite(chapterName);
-  return {assert, extractExamples, ohm, test};
-}
